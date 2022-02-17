3d247e465adbc7f307dd390223737e4924f17bd8
pranta219 <mdpranta219@gmail.com>
2022-02-17 22:47:50 +0600

    button handel

--- js/index.js
+++ js/index.js
@@ -1,15 +1,41 @@
-document.getElementById('button').addEventListener('click', function () {
-    const incomeField = document.getElementById('income-field');
-    const incomeTk = incomeField.value;
+// function calling
+function inputValue(id) {
+    const input = document.getElementById(id)
+    return input
+}
 
-    const foodField = document.getElementById('food-field');
-    const foodTk = foodField.value;
+// calulate button
+inputValue('button').addEventListener('click', function () {
+    const incomeText = inputValue('income-field');
+    const incomeAmount = incomeText.value;
 
-    const rentField = document.getElementById('rent-field');
-    const rentTk = rentField.value;
+    const foodText = inputValue('food-field');
+    const foodAmount = foodText.value;
 
-    const clothesField = document.getElementById('clothes-field');
-    const clothesTk = clothesField.value;
-    const treeItem = foodTk + rentTk + clothesTk;
-    console.log(treeItem)
+    const rentText = inputValue('rent-field');
+    const rentAmount = rentText.value;
+
+    const clothesText = inputValue('clothes-field');
+    const clothesAmount = clothesText.value;
+
+    const expenses = parseFloat(foodAmount) + parseFloat(rentAmount) + parseFloat(clothesAmount)
+
+    const newExpenses = inputValue('expenses')
+    newExpenses.innerText = expenses;
+
+    const totalBalance = incomeAmount - expenses;
+    inputValue('total-amount').innerText = totalBalance;
+
+})
+
+// save button handeler
+inputValue('save-btn').addEventListener('click', function () {
+    const income = inputValue('income-field').value;
+    const balance = inputValue('total-amount').innerText;
+    const persenteceInput = inputValue('save-field').value;
+    const persentValue = (persenteceInput / 100);
+    const savingAmount = income * persentValue;
+    const remainingBalance = balance - savingAmount;
+    inputValue('saving-amount').innerText = savingAmount
+    inputValue('remaining-balance').innerText = remainingBalance
 })
